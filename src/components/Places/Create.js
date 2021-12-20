cd415a6947fbb381b69068d66304cc44be52f463
Kurai Tsukino <sandym3000@hotmail.com>
2021-12-19 20:42:29 -0600

    Vistas de crear y editar lugares

--- src/components/Places/Create.js
+++ src/components/Places/Create.js
@@ -1,9 +1,170 @@
-import React from 'react'
+import React from "react";
 
 export default function Create() {
-    return (
-        <div>
-            Esta es la página para crear lugares.
+  return (
+    <div>
+      <form
+        //onSubmit={ (event) => { handleSubmit(event) }}
+        class="space-y-8 divide-y mr-10 ml-10 divide-gray-200"
+      >
+        <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+          <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
+            <div>
+              <h3 class="text-lg leading-6 font-medium text-gray-900">
+                Create a new place to visit
+              </h3>
+              <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                Full every item below
+              </p>
+            </div>
+            <div class="space-y-6 sm:space-y-5">
+              <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <label
+                  for="first-name"
+                  class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Country of the place
+                </label>
+                <div class="mt-1 sm:mt-0 sm:col-span-2">
+                  <input
+                    //onChange={ (event) => { handleChange(event) } }
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autocomplete="given-name"
+                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+
+              <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <label
+                  for="last-name"
+                  class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Name of the place
+                </label>
+                <div class="mt-1 sm:mt-0 sm:col-span-2">
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autocomplete="family-name"
+                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+
+              <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <label
+                  for="email"
+                  class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Type of attraction
+                </label>
+                <div class="mt-1 sm:mt-0 sm:col-span-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autocomplete="email"
+                    class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+
+              <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <label
+                  for="country"
+                  class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Price in local currency
+                </label>
+                <div class="mt-1 sm:mt-0 sm:col-span-2">
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autocomplete="family-name"
+                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+
+              <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <label
+                  for="street-address"
+                  class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Image of the place
+                </label>
+                <div class="mt-1 sm:mt-0 sm:col-span-2">
+                  <input
+                    type="text"
+                    name="street-address"
+                    id="street-address"
+                    autocomplete="street-address"
+                    class="block max-w-lg w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+
+              <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <label
+                  for="city"
+                  class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Your description. Why is interesting this place?
+                </label>
+                <div class="mt-1 sm:mt-0 sm:col-span-2">
+                  <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    autocomplete="address-level2"
+                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+
+              <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <label
+                  for="region"
+                  class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                >
+                  Map
+                </label>
+                <div class="mt-1 sm:mt-0 sm:col-span-2">
+                  <input
+                    type="text"
+                    name="region"
+                    id="region"
+                    autocomplete="address-level1"
+                    class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+
+        <div class="pt-5">
+          <div class="flex justify-end">
+            <button
+              type="button"
+              class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/components/Places/index.js
+++ src/components/Places/index.js
@@ -12,52 +12,63 @@ export default function Places() {
   useEffect(() => {
     getPlaces();
   }, []);
-  
+
   return (
     <>
-     <div className="max-w-7xl mx-auto my-8 px-4 sm:px-6 lg:px-8" >
-				<div class="md:flex md:items-center md:justify-between">
-					<div class="flex-1 min-w-0">
-						<h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-							Places list
-						</h2>
-					</div>
-				</div>
-
-
-
-				<div class="bg-white">
-					<div>
-						<div class="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-							{
-								places.map(element => {
-									return (
-										<>
-											<div class="" style={{margin: 8}}>
-												<div class="rounded-lg bg-gray-200 aspect-w-1 aspect-h-1 group-hover:opacity-75">
-													<Link to={`/places/${element._id}`} >
-													<img src={element.image} alt="Imagen de lugar" class="w-full h-full object-center object-cover" />
-													</Link>
-												</div>
-												<div class="pt-10 pb-4 text-center">
-													<h3 class="text-sm font-medium text-gray-900">
-														<Link to={`/places/${element._id}`} >
-															{element.name}
-														</Link>
-													</h3>
-													<div class="mt-3 flex flex-col items-center">
-														<p class="mt-1 text-sm text-gray-500">{element.country}</p>
-													</div>
-												</div>
-											</div>
-										</>
-									)
-								})
-							}
-						</div>
-					</div>
-				</div>
-			</div> 
+      <div className="max-w-7xl mx-auto my-8 px-4 sm:px-6 lg:px-8">
+        <div class="md:flex md:items-center md:justify-between">
+          <div class="flex-1 min-w-0">
+            <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+              Places list
+            </h2>
+          </div>
+          <div class="flex justify-end">
+		  <Link to="/places/create">
+            <button
+              type="submit"
+              class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Create a new place
+            </button>
+			</Link>
+          </div>
+        </div>
+        <div class="bg-white">
+          <div>
+            <div class="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              {places.map((element) => {
+                return (
+                  <>
+                    <div class="" style={{ margin: 8 }}>
+                      <div class="rounded-lg bg-gray-200 aspect-w-1 aspect-h-1 group-hover:opacity-75">
+                        <Link to={`/places/${element._id}`}>
+                          <img
+                            src={element.image}
+                            alt="Imagen de lugar"
+                            class="w-full h-full object-center object-cover"
+                          />
+                        </Link>
+                      </div>
+                      <div class="pt-10 pb-4 text-center">
+                        <h3 class="text-sm font-medium text-gray-900">
+                          <Link to={`/places/${element._id}`}>
+                            {element.name}
+                          </Link>
+                        </h3>
+                        <div class="mt-3 flex flex-col items-center">
+                          <p class="mt-1 text-sm text-gray-500">
+                            {element.country}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
